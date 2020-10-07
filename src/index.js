36306e824e9260db041694e8c224959102c22b51
Turbin Alex <turbin28@gmail.com>
2020-10-07 11:31:52 +0300

    feat: tower sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 
 // You should implement your task here.
-
+// console.log("-------------------------------------------------------------")
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    let matrixNew = [];
+    for (i in matrix){
+    //   console.log( i%2 ===0)
+      if ( i%2 ===0 ) {matrix[i].sort(function(a, b) {return a - b;})}
+      else  {matrix[i].sort(function(a, b) {return b - a;})}
+  
+      for(x in matrix[i]){      
+        matrixNew.push(matrix[i][x]);
+      }    
+      }
+  console.log(matrixNew); // [1, 2, 3, 4, 5]
+    return matrixNew;
+  }
